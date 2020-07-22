1975c04ef9522d4e371655f158f133c9ed83fecb
Lisandro Silva <lisandro.t.silva@gmail.com>
2020-07-22 12:22:07 -0300

    Criada buscar na árvore e getJson

--- example/src/App.js
+++ example/src/App.js
@@ -7,7 +7,43 @@ const App = () => {
 
     return (
         <form ref={ref}>
-            <input type='text' />
+            <fieldset name='fieldName2'>
+                <fieldset name='fieldName'>
+                    <input type='text' name='nome1' defaultValue='1' />
+                </fieldset>
+            </fieldset>
+            <div>
+                <input type='text' name='nome2' defaultValue='2' />
+            </div>
+            <div>
+                <span>
+                    <fieldset name='fieldName4'>
+                        <input type='text' name='nome3' defaultValue='3' />
+                    </fieldset>
+                </span>
+            </div>
+
+            <fieldset name='fieldNameComDois'>
+                <input
+                    type='text'
+                    name='filhofieldset'
+                    defaultValue='txugo'
+                ></input>
+
+                <select name='select4' defaultValue='2'>
+                    <option value='1'>1</option>
+                    <option value='2'>2</option>
+                    <option value='3'>3</option>
+                </select>
+
+                <textarea
+                    name='nomeTextArea'
+                    defaultValue={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+                iure quidem laborum, similique, sapiente harum praesentium,
+                veritatis optio inventore expedita sed! Obcaecati libero
+                temporibus animi reprehenderit ipsam ab aliquam modi.`}
+                ></textarea>
+            </fieldset>
         </form>
     );
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,51 @@ export const useSimpleForm = () => {
 
     useEffect(() => {
         if (refForm.current !== null) {
-            // refForm.current;
+            const formClone = refForm.current;
+
+            const mapDOM = (element, json) => {
+                var treeObject = {};
+
+                function treeHTML(element, object) {
+                    if (
+                        element.nodeName === 'INPUT' ||
+                        element.nodeName === 'SELECT' ||
+                        element.nodeName === 'TEXTAREA'
+                    ) {
+                        object[element.getAttribute('name')] = element.value;
+                    }
+
+                    const nodeList = element.childNodes;
+                    if (nodeList != null) {
+                        if (nodeList.length) {
+                            for (var i = 0; i < nodeList.length; i++) {
+                                if (
+                                    nodeList[i].nodeType ===
+                                    nodeList[i].ELEMENT_NODE
+                                ) {
+                                    if (element.nodeName === 'FIELDSET') {
+                                        const name = element.getAttribute(
+                                            'name'
+                                        );
+                                        if (!object[name]) {
+                                            object[name] = {};
+                                        }
+                                        treeHTML(nodeList[i], object[name]);
+                                    } else {
+                                        treeHTML(nodeList[i], object);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                treeHTML(element, treeObject);
+
+                return json ? JSON.stringify(treeObject) : treeObject;
+            };
+
+            // refForm.current = Form
+            refForm.current.getJson = () => mapDOM(formClone);
         }
     });
 
